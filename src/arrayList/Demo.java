96d2009b90ff5fbc232f43dd03a01919c66898ca
Subhadra27 <135145628+Subhadra27@users.noreply.github.com>
2024-02-05 20:17:29 +0530

    Update Demo.java

--- src/arrayList/Demo.java
+++ src/arrayList/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	int i = 100;
 
 	public static void main(String[] args) {
-		System.out.println("Demo array list");
+		System.out.println("Demo array list from master remote");
 
 	}
 
